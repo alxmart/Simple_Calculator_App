5c09a92888533897d894bca1e015d5051032b66a
alxmart <alxmartinez@gmail.com>
2023-01-07 19:33:50 -0300

    Added btn_plus.setOnClickListener

--- app/src/main/java/com/androidatc/simplecalculatorapp/MainActivity.kt
+++ app/src/main/java/com/androidatc/simplecalculatorapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.androidatc.simplecalculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val n1 = et_Number1.text
+        val n2 = et_Number2.text
+
+        btn_plus.setOnClickListener {
+            val sumResult = n1.toString().toDouble() +
+                            n2.toString().toDouble()
+            tv_result_view.text = sumResult.toString()
+        }
+
+
     }
 }
